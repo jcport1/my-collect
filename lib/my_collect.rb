94c558eb1157a2469e4df16f31040d17420bada7
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-10-26 20:48:40 -0400

    commit tree 4659743ea1c14720aa804fb7192e9388263235e8

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 #build a method that yields members of a collection to a block
 #control the return value of a mthod the uses yield such that it returns a new collection
 
-def my_collect(argument)
+def my_collect(array)
 
 collection = []
 i = 0
-    while i < argument.length
-    collection << yield(argument[i])
+    while i < array.length
+    collection << yield(array[i])
     i += 1
     end
     collection 
