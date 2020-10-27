cc9d9c026ac926743c0322e993c899b3a5a2facd
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-10-26 20:41:08 -0400

    testing

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+#build a method that yields members of a collection to a block
+#control the return value of a mthod the uses yield such that it returns a new collection
+
+def my_collect(argument)
+
+collection = []
+i = 0
+    while i < argument.length
+    collection << yield(argument[i])
+    i += 1
+    end
+    collection 
+end 
+
 
 
